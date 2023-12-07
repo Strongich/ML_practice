f4ab1d275e8ab0e421a4a4ca30a1609b3481a797
Strongich <igor.vitenko13@gmail.com>
2023-12-07 20:05:23 +0200

    deleted config change per run on inference

--- task1/inference.py
+++ task1/inference.py
@@ -1,12 +1,5 @@
-import json
 from transformers import AutoModelForTokenClassification, AutoTokenizer
 from transformers import pipeline
-config = json.load(open("ner_model/config.json"))
-label2id = {'B-Mountain': 0, 'I-Mountain': 1, 'O': 2}
-id2label = {0: 'B-Mountain', 1: 'I-Mountain', 2: 'O'}
-config["id2label"] = id2label
-config["label2id"] = label2id
-json.dump(config, open("ner_model/config.json","w"))
 
 def run_inference(example):
     model_fine_tuned = AutoModelForTokenClassification.from_pretrained('Strongich/bert-english-ner-mountain')
